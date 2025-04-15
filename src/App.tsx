fde4158bad3105ecfd96a8bf51f3a27a283dd7a5
Gnanesh Andra <andragnanesh@gmail.com>
2025-04-15 15:25:08 -0500

    profile somewhat working
    profile working

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/profile";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} exact={true} />
+      <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/profile" element={<Profile />} /> 
     </Routes>
   );
 }
